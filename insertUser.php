157fd3517d1c73c4760f07c6c88b8ace33593701
NightmareRevisited <417045158@qq.com>
2017-11-27 14:50:55 +0800

    Revert "Initial commit"
    This reverts commit fe9dd9e3b9232ebcf4bc7d749bdb2404440846d6.

--- insertUser.php
+++ insertUser.php
@@ -4,39 +4,47 @@
  * Author: Yang Changning (thevile@126.com)
  * Time: 2017/11/26 16:34
  */
-
 $servername = "localhost:3306";
 $username = "root";
 $password = "Diabl0s3";
 $dbname = "microblog";
-
 $conn = new mysqli($servername,$username,$password,$dbname);
-
 if ($conn->connect_error) {
     die("连接失败:".$conn->connect_error);
 }
-
-
 $username = $_POST['username'];
 $password = $_POST['password'];
-
 $sql1 = "SELECT * FROM login_info where username='$username' ";
 $result = $conn->query($sql1);
-
 if ($result->num_rows > 0) {
-    echo "注册失败，该用户名已存在";
+    $message = "注册失败，该用户名已存在"."<br>"."3秒后返回注册页面……";
+    $url = "http://microblog.com/register.php";
 }
 else {
-
     $sql2 = "INSERT INTO login_info (username,password) VALUES ('$username','$password')";
-
     if ($conn->query($sql2) === TRUE) {
-        echo "注册成功" . PHP_EOL;
+        $message = "注册成功" ."<br>"."3秒后返回登陆页面……";
+        $url = "http://microblog.com/login.php";
     } else {
         echo "Error:" . $sql . "<br>" . $conn->error;
     }
 }
-
 $conn->close();
-
 ?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>登陆界面</title>
+    <meta  http-equiv = "refresh"   content ="3;
+    url = <?php echo $url;  ?> " >
+</head>
+<body>
+
+<?php
+echo $message;
+?>
+
+
+</body>
+</html>
